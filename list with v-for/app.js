bcf269b2ae7aa2ccf264c01438e27c5b795ac09b
Guilherme Oliveira Epifanio <gui.epifanio@gmail.com>
2018-07-07 19:10:40 +1000

    new exercises

---
+++ list with v-for/app.js
@@ -0,0 +1,10 @@
+new Vue({
+  el: '#app',
+  data: {
+    ingredients: ['meat', 'fruit', 'cookies'],
+    persons: [
+      {name: 'Gui', age: 27, color: 'red'},
+      {name: 'Bia', age: 'unknown', color: 'blue'}
+    ]
+  }
+});
